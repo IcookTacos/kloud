39c6ede1f43177927255b1bb2b51009df160a765
IcookTacos <zeidlitzerik@gmail.com>
2024-01-29 03:43:50 -0500

    Add service

--- run.sh
+++ run.sh
@@ -6,10 +6,6 @@ echo "Creating context\n"
 kubectl config set-context developer --cluster kubernetes --user martin
 sleep 0.12
 
-echo "Switching context to \"developer\"\n"
-kubectl config use-context developer
-sleep 0.12
-
 echo "Creating role\n"
 kubectl apply -f role.yaml
 sleep 0.12
@@ -17,4 +13,11 @@ sleep 0.12
 echo "Creating role-binding\n"
 kubectl apply -f rb.yaml
 sleep 0.12
+
+echo "Switching context to \"developer\"\n"
+kubectl config use-context developer
+sleep 0.12
+
+echo "Creating service \"Jekyll\""
+kubectl apply -f svc.yaml
 
