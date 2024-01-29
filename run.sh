e3047a43a14ca79740875f2824d5a2b18be2041c
IcookTacos <zeidlitzerik@gmail.com>
2024-01-29 02:55:05 -0500

    Add role.yaml

--- run.sh
+++ run.sh
@@ -2,3 +2,7 @@ echo "Creating user"
 kubectl config set-credentials martin --client-key /root/martin.key --client-certificate /root/martin.crt
 echo "Creating context"
 kubectl config set-context developer --cluster kubernetes --user martin
+echo "Creating namespace \"developer\""
+kubectl create ns developer
+echo "Creating role"
+kubectl apply -f role.yaml
