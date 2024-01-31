972b177fe2118c728e0ff73207e32dd684ffae0a
IcookTacos <zeidlitzerik@gmail.com>
2024-01-31 03:09:58 -0500

    Amending run.sh

--- task-3/run.sh
+++ task-3/run.sh
@@ -6,3 +6,6 @@ echo "Setting default namespace to \"vote\""
 kubectl config set-context --current --namespace vote
 sleep 0.25
 
+echo "Provisioning resrouces:"
+kubectl apply -f vote-service.yaml
+sleep 0.25
